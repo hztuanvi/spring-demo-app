e8af2336fbfc963ebdd180b6fc9efdb01d75e377
thutrang28022000 <phamthutrang280200@gmail.com>
2020-12-18 22:02:06 +0700

    asdfghjk

--- JavaCore/src/javacore/JavaCore.java
+++ JavaCore/src/javacore/JavaCore.java
@@ -15,7 +15,6 @@ public class JavaCore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
