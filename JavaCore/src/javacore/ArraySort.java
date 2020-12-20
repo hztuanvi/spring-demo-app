a0552211521327ce37caaacedc5557ecede86fb4
HANZZY\ADMIN <hanzzy111@gmail.com>
2020-12-20 16:12:08 +0700

    Fix conding convention

--- JavaCore/src/javacore/ArraySort.java
+++ JavaCore/src/javacore/ArraySort.java
@@ -1,52 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javacore;
-
 
 import java.util.Scanner;
 
 /**
+ * Example about array sort
+ * 
+ * @author TrangPT
  *
- * @author PC
  */
 public class ArraySort {
-     
-     //sắp xếp mảng theo chiều tăng dần.
-    public static void sort(int[] array){
-    for (int i = 0; i <array.length; i++) {
-             for (int j=i+1; j <array.length; j++) 
-                 if(array[i]>array[j])
-                 {
-                   int t=array[i];
-                   array[i]=array[j];
-                   array[j]=t;
-                 }
-             
-         }    
-    }
-    //in ra màn hình.
-    public static void output(int[] array){
-       System.out.println("ArraySort");
-         for (int i = 0; i <array.length; i++) {
-             System.out.print(" "+array[i]); 
-         } 
-    }
-    static Scanner input = new Scanner(System.in);  
-    public static void main(String[] args) {    
-        System.out.println("Nhap so phan tu trong mang:");
-        int n = input.nextInt();               
-        int [] array = new int [n];
-        System.out.print("Nhap cac phan tu trong mang: \n");
-        for (int i = 0; i < n; i++) {
-            System.out.printf("a[%d] = ", i);
-            array[i] = input.nextInt();
-        }       
-       sort(array);
-       output(array);        
-     }
-     
-    
+
+	public static void main(String[] args) {
+		Scanner sCInput = new Scanner(System.in);
+		System.out.print("Nhap so phan tu trong mang: ");
+		int numberOfElement = sCInput.nextInt();
+		int[] array = new int[numberOfElement];
+		
+		System.out.print("Nhap cac phan tu trong mang:\n");
+		
+		for (int i = 0; i < numberOfElement; i++) {
+			System.out.printf("a[%d] = ", i);
+			array[i] = sCInput.nextInt();
+		}
+		
+		sort(array);
+		output(array);
+		
+		// Close scanner
+		sCInput.close();
+	}
+
+	/**
+	 * Sorts the array in ascending order
+	 * @param array array to sort 
+	 */
+	private static void sort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++)
+				if (array[i] > array[j]) {
+					int t = array[i];
+					array[i] = array[j];
+					array[j] = t;
+				}
+		}
+	}
+
+	/**
+	 * Output array after sort
+	 * @param array array to output
+	 */
+	private static void output(int[] array) {
+		System.out.println("Mang sau khi sort tang dan:");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+
 }
